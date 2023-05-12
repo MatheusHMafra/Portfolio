f842e84b50e13521e1578b83fcc1879244688228
Matheus <127265280+MatheusHMafra@users.noreply.github.com>
2023-05-12 01:03:47 -0300

    Teste do contate-me

--- script.js
+++ script.js
@@ -79,3 +79,46 @@ function toggleMode() {
     updateProjetos();
     updateMiniProjetos();
 }
+
+// Contate-me parte
+
+const webhookUrl = process.env.WEBHOOK_URL;
+
+function enviarMensagem() {
+  const nome = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const assunto = document.getElementById("subject").value;
+  const mensagem = document.getElementById("message").value;
+  
+  const mensagemJson = {
+    username: '$(nome)',
+    embeds: [
+      {
+        title: $(assunto),
+        description: $(mensagem),
+        footer: {
+          text: `Email: ${email}`
+        }
+      }
+    ]
+  };
+
+  fetch(webhookUrl, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(mensagemJson),
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Erro ao enviar a mensagem.");
+      }
+      console.log("Mensagem enviada com sucesso!");
+      document.getElementById("name").value = "";
+      document.getElementById("email").value = "";
+      document.getElementById("subject").value = "";
+      document.getElementById("message").value = "";
+    })
+    .catch((error) => {
+      console.error("Erro ao enviar a mensagem:", error);
+    });
+}
